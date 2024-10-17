6eeea62452739620f9593fa446a6c3a91f07f104
tothdodo <tothdodo01@gmail.com>
2024-10-17 21:52:59 +0200

    Add product and list them

---
+++ src/app/admin/products/_components/ProductActions.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { DropdownMenuItem } from "@/components/ui/dropdown-menu";
+import { useTransition } from "react";
+import { toggleProductAvailability, deleteProduct } from "../../_actions/products";
+
+export function ActiveToggleDropdownItem({
+    id, isAvailableForPurchase }: { id: string, isAvailableForPurchase: boolean }) {
+    const [isPending, startTransition] = useTransition();
+
+    return (
+        <DropdownMenuItem
+            disabled={isPending}
+            onClick={() => {
+                startTransition(async () => {
+                    await toggleProductAvailability(id, !isAvailableForPurchase);
+                })
+            }}
+        >
+            {isAvailableForPurchase ? "Deactivate" : "Activate"}
+        </DropdownMenuItem>
+    );
+}
+
+export function DeleteDropdownItem({ id, disabled }: { id: string, disabled: boolean }) {
+    const [isPending, startTransition] = useTransition();
+
+    return (
+        <DropdownMenuItem
+            variant="destructive"
+            disabled={isPending || disabled}
+            onClick={() => {
+                startTransition(async () => {
+                    await deleteProduct(id);
+                })
+            }}
+        >
+            Delete
+        </DropdownMenuItem>
+    );
+}
