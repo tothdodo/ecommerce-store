36b6be3d4d89d78077723f84808fbcc3af690513
tothdodo <tothdodo01@gmail.com>
2025-01-19 21:13:30 +0100

    Stripe integration

---
+++ src/app/(customerFacing)/stripe/purchase-success/page.tsx
@@ -0,0 +1,5 @@
+export default function SuccessPage() {
+    return (
+        <h1>Success!!!</h1>
+    )
+}
---
+++ src/app/actions/orders.ts
@@ -0,0 +1,13 @@
+"use server";
+
+import db from "@/db/db";
+
+export async function userOrderExist(email: string, productId: string) {
+    return (await db.order.findFirst({
+        where: {
+            user: {email},
+            productId
+        },
+        select: {id: true}
+    })) != null;
+}
