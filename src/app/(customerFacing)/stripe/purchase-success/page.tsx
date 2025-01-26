7b475791f5f91f2009b97ee923de0b5403315295
tothdodo <tothdodo01@gmail.com>
2025-01-26 13:01:58 +0100

    Handling payment and download the purchased file

--- src/app/(customerFacing)/stripe/purchase-success/page.tsx
+++ src/app/(customerFacing)/stripe/purchase-success/page.tsx
@@ -1,5 +1,71 @@
-export default function SuccessPage() {
+import { Button } from '@/components/ui/button';
+import db from '@/db/db';
+import { formatCurrency } from '@/lib/formatters';
+import Image from 'next/image';
+import Link from 'next/link';
+import { notFound } from 'next/navigation';
+import Stripe from 'stripe';
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
+
+export default async function SuccessPage({ searchParams }:
+    { searchParams: { payment_intent: string } }
+) {
+    const paymentIntent = await stripe.paymentIntents.retrieve(searchParams.payment_intent);
+
+    if (paymentIntent.metadata.productId == null) return notFound();
+
+    const product = await db.product.findUnique({
+        where: {
+            id: paymentIntent.metadata.productId
+        }
+    });
+
+    if (product == null) return notFound();
+
+    const isSuccess = paymentIntent.status === 'succeeded';
+
     return (
-        <h1>Success!!!</h1>
+        <div className='max-w-5xl w-full mx-auto space-y-7'>
+            <h1 className='text-4xl font-bold'>{isSuccess ? "Success!" : "Error!"}</h1>
+            <div className='flex gap-4 items-center'>
+                <div className='aspect-video flex-shrink-0 w-1/3 relative'>
+                    <Image src={product.imagePath} alt={product.name} fill />
+                </div>
+                <div>
+                    <div className="text-lg">
+                        {formatCurrency(product.priceInCents / 100)}
+                    </div>
+                    <h1 className="text-2xl font-bold">
+                        {product.name}
+                    </h1>
+                    <div className="line-clamp-3 text-muted-foreground">
+                        {product.description}
+                    </div>
+                    <Button className='mt-4' size="lg" asChild>
+                        {
+                            isSuccess ?
+                                <a href={`/products/download/${await createDownloadVerificationId(product.id)}`}>
+                                    Download
+                                </a>
+                                :
+                                <Link href={`/products/${product.id}/purchase`}>
+                                    Try again
+                                </Link>
+                        }
+                    </Button>
+                </div>
+            </div>
+        </div >
     )
+}
+
+async function createDownloadVerificationId(productId: string) {
+    return (
+        await db.downloadVerification.create({
+            data: {
+                productId: productId,
+                expiresAt: new Date(Date.now() + 1000 * 60 * 60 * 24)
+            }
+        })).id;
 }
